501ab3355314ad2267ad9f27fdcd22e335a1c8c2
BMinin <boris.minin@outlook.com>
2023-12-20 12:18:58 +0500

    Some changes

--- TechnicalMaintenanceMiddleware_Sample/Controllers/WeatherForecastController.cs
+++ TechnicalMaintenanceMiddleware_Sample/Controllers/WeatherForecastController.cs
@@ -21,13 +21,17 @@ namespace TechnicalMaintenanceMiddleware_Sample.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            _logger.LogInformation("Completed successfully");
+
+            return result;
         }
     }
 }
